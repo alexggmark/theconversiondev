55e28366289ce856c68c449400cd6fc16d6e188e
Alex <alexgmark@gmail.com>
2025-11-18 23:26:48 +0000

    Changing theme

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   },
   markdown: {
     shikiConfig: {
-      theme: 'css-variables',
+      theme: 'github-dark',
       wrap: false
     },
     remarkPlugins: [remarkMath, remarkDirective, remarkEmbeddedMedia, remarkReadingTime, remarkTOC],
